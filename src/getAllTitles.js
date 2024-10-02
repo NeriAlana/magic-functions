452746263690acbc47e55d0ba2637a4ed2ee0af3
AlanaNeri <152909620+NeriAlana@users.noreply.github.com>
2024-10-02 10:37:51 -0300

    Update getAllTitles.js
    A função getAllTitles é responsável por recuperar um array de títulos de filmes a partir de uma lista de filmes fornecida.

--- src/getAllTitles.js
+++ src/getAllTitles.js
@@ -1,9 +1,14 @@
+const { list1 } = require("../data/list1");
 const movies = require("../data/movies");
 
 const getAllTitles = (movies) => {
- 
+    let filtrar = movies.filter((movie) => movie.titulo)
+    .map((movie) => movie.titulo);
+    return filtrar;  // Retornar um array com os títulos dos filmes
+    
 }
 
 module.exports = {
   getAllTitles
 }
+console.log(getAllTitles(movies));
